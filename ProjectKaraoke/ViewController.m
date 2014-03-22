c290d8e8bc36f3d99c116d61e3ff895462769210
Lance Fu <lancefu@gmail.com>
2014-03-21 18:32:57 -0700

    Added start and end recording in ViewController

--- ProjectKaraoke/ViewController.m
+++ ProjectKaraoke/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "AERecorder.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UIButton *startRecordingButton;
+
+@property (readonly, nonatomic) AEAudioController *audioController;
+@property (strong, nonatomic) AERecorder *recorder;
 
 @end
 
@@ -17,6 +23,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 	
+    self.navigationController.navigationBar.hidden = YES;
+    
+    self.startRecordingButton.layer.cornerRadius = 8.0f;
+    self.startRecordingButton.layer.borderWidth = 1.0f;
+    self.startRecordingButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
 }
 
 
@@ -24,5 +35,51 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark - Accessor
+
+- (AEAudioController *)audioController {
+    return ((AppDelegate *)([UIApplication sharedApplication].delegate)).audioController;
+}
+
+
+#pragma mark - Private Methods
+
+- (IBAction)startRecording:(id)sender {
+    if (!self.recorder) {
+//        NSBundle *mainBundle = [NSBundle mainBundle];
+//        NSString *filePath = [mainBundle pathForResource: @"test" ofType: @"wav"];
+        NSLog(@"Start Recording");
+        self.recorder = [[AERecorder alloc] initWithAudioController:self.audioController];
+        NSString *documentsFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        NSString *filePath = [documentsFolder stringByAppendingPathComponent:@"Recording.aiff"];
+        NSError *error = NULL;
+        if (![_recorder beginRecordingToFileAtPath:filePath fileType:kAudioFileAIFFType error:&error]) {
+            NSLog(@"Error: Failed to begin recording: %@", error);
+            return;
+        }
+        
+        [self.audioController addInputReceiver:self.recorder];
+        [self.audioController addOutputReceiver:self.recorder];
+        
+        [self.startRecordingButton setTitle:@"End Recording" forState:UIControlStateNormal];
+    }
+    else {
+        NSLog(@"End Recording");
+        [self endRecording];
+        
+        [self.startRecordingButton setTitle:@"Start Recording" forState:UIControlStateNormal];
+    }
+}
+
+
+- (void)endRecording {
+    [self.audioController removeInputReceiver:self.recorder];
+    [self.audioController removeOutputReceiver:self.recorder];
+    [self.recorder finishRecording];
+    self.recorder = nil;
+}
+
 
 @end
