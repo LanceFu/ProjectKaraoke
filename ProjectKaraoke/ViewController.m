d6fbbca61263ea35ce4a8b9eef1f9ba556ae10dc
Lance Fu <lancefu@gmail.com>
2014-03-21 20:12:38 -0700

    Added play functions  & basic UI

--- ProjectKaraoke/ViewController.m
+++ ProjectKaraoke/ViewController.m
@@ -11,9 +11,12 @@
 
 @interface ViewController ()
 
-@property (strong, nonatomic) IBOutlet UIButton *startRecordingButton;
+@property (strong, nonatomic) IBOutlet UIButton *recordButton;
+@property (strong, nonatomic) IBOutlet UIButton *playButton;
 
 @property (readonly, nonatomic) AEAudioController *audioController;
+@property (strong, nonatomic) AEAudioFilePlayer* audioSourceFilePlayer;
+@property (strong, nonatomic) AEAudioFilePlayer* audioOutputFilePlayer;
 @property (strong, nonatomic) AERecorder *recorder;
 
 @end
@@ -25,9 +28,17 @@
 	
     self.navigationController.navigationBar.hidden = YES;
     
-    self.startRecordingButton.layer.cornerRadius = 8.0f;
-    self.startRecordingButton.layer.borderWidth = 1.0f;
-    self.startRecordingButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    self.recordButton.layer.cornerRadius = 8.0f;
+    self.recordButton.layer.borderWidth = 1.0f;
+    self.recordButton.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    
+    // Read the file from within project
+    NSURL *filePath = [[NSBundle mainBundle] URLForResource: @"test" withExtension: @"wav"];
+    self.audioSourceFilePlayer = [AEAudioFilePlayer audioFilePlayerWithURL:filePath audioController:self.audioController error:NULL];
+    __weak ViewController *weakSelf = self;
+    self.audioOutputFilePlayer.completionBlock = ^{
+        [weakSelf endRecording];
+    };
 }
 
 
@@ -46,40 +57,80 @@
 
 #pragma mark - Private Methods
 
-- (IBAction)startRecording:(id)sender {
+- (IBAction)recordingAction:(id)sender {
     if (!self.recorder) {
-//        NSBundle *mainBundle = [NSBundle mainBundle];
-//        NSString *filePath = [mainBundle pathForResource: @"test" ofType: @"wav"];
-        NSLog(@"Start Recording");
-        self.recorder = [[AERecorder alloc] initWithAudioController:self.audioController];
-        NSString *documentsFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-        NSString *filePath = [documentsFolder stringByAppendingPathComponent:@"Recording.aiff"];
-        NSError *error = NULL;
-        if (![_recorder beginRecordingToFileAtPath:filePath fileType:kAudioFileAIFFType error:&error]) {
-            NSLog(@"Error: Failed to begin recording: %@", error);
-            return;
-        }
-        
-        [self.audioController addInputReceiver:self.recorder];
-        [self.audioController addOutputReceiver:self.recorder];
-        
-        [self.startRecordingButton setTitle:@"End Recording" forState:UIControlStateNormal];
+        NSLog(@"======== Start Recording ========");
+        [self startRecording];
     }
     else {
-        NSLog(@"End Recording");
+        NSLog(@"======== End Recording ========");
         [self endRecording];
-        
-        [self.startRecordingButton setTitle:@"Start Recording" forState:UIControlStateNormal];
     }
+}
+
+
+- (void)startRecording {
+    self.playButton.hidden = YES;
+    self.recorder = [[AERecorder alloc] initWithAudioController:self.audioController];
+    NSString *documentsFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [documentsFolder stringByAppendingPathComponent:@"Recording.aiff"];
+    NSError *error = NULL;
+    if (![_recorder beginRecordingToFileAtPath:filePath fileType:kAudioFileAIFFType error:&error]) {
+        NSLog(@"Error: Failed to begin recording: %@", error);
+        return;
+    }
+    [self.audioController addChannels:@[self.audioSourceFilePlayer]];
+    [self.audioController addInputReceiver:self.recorder];
+    [self.audioController addOutputReceiver:self.recorder];
+    
+    [self.recordButton setTitle:@"End Recording" forState:UIControlStateNormal];
 }
 
 
 - (void)endRecording {
+    [self.audioController removeChannels:@[self.audioSourceFilePlayer]];
+    self.audioSourceFilePlayer.currentTime = 0.0;
+    
     [self.audioController removeInputReceiver:self.recorder];
     [self.audioController removeOutputReceiver:self.recorder];
     [self.recorder finishRecording];
+    
+    self.playButton.hidden = NO;
     self.recorder = nil;
+    
+    [self.recordButton setTitle:@"Start Recording" forState:UIControlStateNormal];
 }
 
+
+- (IBAction)playAction:(id)sender {
+    if (self.audioOutputFilePlayer.channelIsPlaying) {
+        NSLog(@"======== Stop Playing ========");
+        [self stopAudio];
+    }
+    else {
+        NSLog(@"======== Start Playing ========");
+        [self playAudio];
+    }
+}
+
+
+- (void)playAudio {
+    [self.playButton setImage:[UIImage imageNamed:@"stop_icon"] forState:UIControlStateNormal];
+    NSString *documentsFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSURL *filePath = [NSURL URLWithString:[documentsFolder stringByAppendingPathComponent:@"Recording.aiff"]];
+    self.audioOutputFilePlayer = [AEAudioFilePlayer audioFilePlayerWithURL:filePath audioController:self.audioController error:NULL];
+    __weak ViewController *weakSelf = self;
+    self.audioOutputFilePlayer.completionBlock = ^{
+        [weakSelf stopAudio];
+    };
+    [self.audioController addChannels:@[self.audioOutputFilePlayer]];
+}
+
+
+- (void)stopAudio {
+    [self.playButton setImage:[UIImage imageNamed:@"play_icon"] forState:UIControlStateNormal];
+    [self.audioController removeChannels:@[self.audioOutputFilePlayer]];
+    self.audioOutputFilePlayer.currentTime = 0.0;
+}
 
 @end
