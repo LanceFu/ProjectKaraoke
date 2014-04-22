c0781042df7b699b871b9a0d1e1f02a6e812beb2
Lance Fu <lancefu@gmail.com>
2014-04-22 12:03:06 -0700

    Added Facebook SDK framework to project

--- ProjectKaraoke/SharedConstants.h
+++ ProjectKaraoke/SharedConstants.h
@@ -12,4 +12,5 @@
 #define APPHANCE_APP_KEY                    @"fe34d6ce87d40d498945285318588f0a93fb339d"
 #define SOUNDCLOUD_CLIENT_ID                @"4a16af6d8a5d801d1efd30491f2255b6"
 #define SOUNDCLOUD_CLIENT_SECRET            @"7fdbb231fbc16591bf1098bfec04b216"
-
+#define FACEBOOK_APP_ID                     @"1429822987268451"
+#define FACEBOOK_APP_SECRET                 @"1d2eedd434d804e8c509813f3681d2e7"
